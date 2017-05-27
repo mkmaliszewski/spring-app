f4124186c26658b551292059ccdf63be149eb214
Marek Maliszewski <mkmaliszewski@gmail.com>
2017-05-27 16:13:07 +0200

    added a model class contact

---
+++ src/main/java/model/Contact.java
@@ -0,0 +1,80 @@
+package model;
+
+import java.io.Serializable;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table (name = "contacts")
+public class Contact implements Serializable{
+    
+    @Id
+    private int id;
+    
+    @Column
+    private String name;
+    
+    @Column
+    private String lastName;
+    
+    @Column
+    private String mail;
+    
+    @Column
+    private String number;
+    
+    @Column
+    private String bornDate;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getMail() {
+        return mail;
+    }
+
+    public void setMail(String mail) {
+        this.mail = mail;
+    }
+
+    public String getNumber() {
+        return number;
+    }
+
+    public void setNumber(String number) {
+        this.number = number;
+    }
+
+    public String getBornDate() {
+        return bornDate;
+    }
+
+    public void setBornDate(String bornDate) {
+        this.bornDate = bornDate;
+    }
+    
+    
+}
